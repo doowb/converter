c00fad6b76bdce9b9192dd41506150b1e77ee3ea
Hariadi Hinta <diperakui@yahoo.com>
2013-12-24 22:53:29 +0800

    remove old xml builder function

--- src/convert.js
+++ src/convert.js
@@ -34,55 +34,6 @@ var extend = function(options) {
 	return opts;
 };
 
-var toXml = function xml(json, options) {
-	var XML_CHARACTER_MAP = {
-		'&': '&amp;',
-		'"': '&quot;',
-		"'": '&apos;',
-		'<': '&lt;',
-		'>': '&gt;'
-	};
-	var result = options.header ? '<?xml version="1.0" encoding="UTF-8"?>' : '';
-	var type = json.constructor.name;
-
-	options.header = false;
-
-	if(type === 'Array') {
-		json.forEach(function(node) {
-			result += xml(node, options);
-		});
-	} else if (type === 'Object' && typeof json === 'object') {
-
-		Object.keys(json).forEach(function(key) {
-			if (key !== options.attrkey) {
-				var node = json[key];
-				var attributes = '';
-
-				if (options.attrkey && json[options.attrkey]) {
-					Object.keys(json[options.attrkey]).forEach(function(k) {
-						attributes += util.format(' %s="%s"', k, json[options.attrkey][k]);
-					});
-				}
-				var inner = xml(node, options);
-
-				if (inner) {
-					result += util.format("<%s%s>%s</%s>", key, attributes, xml(node, options), key);
-				} else {
-					result += util.format("<%s%s/>", key, attributes);
-				}
-			}
-		});
-
-	} else {
-		return json.toString()
-			.replace(/([&"<>''])/g, function(str, item) {
-				return XML_CHARACTER_MAP[item];
-			});
-	}
-
-	return result;
-};
-
 var convertCsvToJson = function (data, options, done) {
 	csv()
 		.from(data, options.csv)
